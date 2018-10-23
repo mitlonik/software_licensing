bd5f1faf12783ec8053675e3db867c432eda4468
Dmitry Zavedeev <emachines.dima@yandex.ru>
2018-10-23 16:32:22 +0300

    sourse files
    requirement:
    "Консольная программа на языке С++, реализующая
    следующую функциональность:
    
    input.txt:
    <Строка длиной до 256 произвольных символов>
    
    output.txt:
    В случае, если исходная строка начинается с заглавной латинской буквы, то вывести исходную строку с заменой всех букв на заглавные. (случай А)
    В противном случае вывести строку с заменой всех букв на строчные. (случай Б)
    
    input - for the case of A 
    input1 is an arbitrary
    inputB-case B

---
+++ Header.h
@@ -0,0 +1,6 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <fstream>	// to read data from a file 
+#include <conio.h>	//for _getch(), do not close window
+#include <ctime>	// function of time
+#include "windows.h"
